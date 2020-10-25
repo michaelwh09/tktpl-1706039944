0d909fd5e496f0a9d8ca78477158163d408b7be4
Michael Halim <halimmichael09@gmail.com>
2020-10-25 20:09:23 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelwiryadinatahalim/chatapp/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.mobileprogramming.michaelwiryadinatahalim.chatapp.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
