b26458febc2d269a83d7bc0cb490934e507b42b7
Michael Halim <halimmichael09@gmail.com>
2020-11-02 17:52:46 +0700

    Remove safe call

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelwiryadinatahalim/chatapp/ui/main/FriendAdapter.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelwiryadinatahalim/chatapp/ui/main/FriendAdapter.kt
@@ -37,7 +37,7 @@ class FriendAdapter : PagingDataAdapter<UserAndRoomChat, FriendAdapter.FriendVie
     companion object {
         private val diffCallback = object : DiffUtil.ItemCallback<UserAndRoomChat>() {
             override fun areItemsTheSame(oldItem: UserAndRoomChat, newItem: UserAndRoomChat): Boolean =
-                oldItem.user?.uid == newItem.user?.uid && oldItem.roomChat?.uid == newItem.roomChat?.uid
+                oldItem.user.uid == newItem.user.uid && oldItem.roomChat?.uid == newItem.roomChat?.uid
 
             override fun areContentsTheSame(oldItem: UserAndRoomChat, newItem: UserAndRoomChat): Boolean =
                 oldItem == newItem
